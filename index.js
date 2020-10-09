f7032c62fd1fd5684d9bd05808a5cc0d544f3e6c
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-10-09 19:59:43 -0400

    first commit with all the files
    I added all the necesary information to make the code work I still didn't add essential information to work but with this information, I have a template to make the code work...

---
+++ index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+
+const generatePage = require('./src/html-template.js');
+
+// const [username, email, title, description, license, install, test, message, contribution] = answers;
+
+const inquirer = require("inquirer");
+
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt([
+
+            {type: "input",
+                name: "name",
+                message: "What is your name?",
+            },
+
+            {type: "input",
+                name: "username",
+                message: "What is your Github username?",
+            },
+
+            {type: "input",
+                name: "email",
+                message: "What is your email address?",
+            },
+
+            {type: "input",
+                name: "title",
+                message: "What is your project's name?",
+            },
+
+            {type: "input",
+                name: "description",
+                message: "Please write a short description of your project",
+            },
+
+            {type: "input",
+                name: "install",
+                message: "What command should be run to install dependencies?",
+            },
+
+            {type: "input",
+                name: "test",
+                message: "what command should be run to run test?",
+            },
+
+            {type: "input",
+                name: "message",
+                message: "what does the user need to know about using the repo?",
+            },
+
+            {type: "input",
+                name: "contribution",
+                message: "What does the user need to know about contributing to the repo",
+            },
+
+    ]);
+};
+
+questions().then((answers) => {
+    console.log(answers);
+  
+    var template = generatePage(answers);
+    writeToFile("./dist/index.html", template);
+  });
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      return console.log("Success! Look at your index.md");
+    });
+  } 
