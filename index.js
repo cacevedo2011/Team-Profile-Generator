5977ba2726c8505a31941f918d6a64137be400c8
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-10-11 21:41:27 -0400

    Final Details
    All of the challenge is all done everything works like it suppose to do and including on the readme, I will add the link of the video with the screenshot of the output of the code and how it looks on the html.  I also added the license I use to make this code happen.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
-const render = require('./src/html-template');
+const render = require('./src/html-template.js');
+const path = require("path");
 
 // const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
+const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const outputPath = path.join(OUTPUT_DIR, "project.html");
 
 const inquirer = require("inquirer");
 const teamMembers = [];
@@ -15,11 +18,6 @@ const Menu = () => {
 
     const generateQuestions = () => {
         return inquirer.prompt([
-
-            {type: "input",
-                name: "project",
-                message: "What is the project's name?",
-            },
 
             {type: "input",
                 name: "managerName",
@@ -45,21 +43,19 @@ const Menu = () => {
         const manager = new Manager (answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice);
         teamMembers.push(manager);
         idArray.push(answers.managerId);
-        createTeam();
+        generateTeam();
         });
     };
 
-    const newLocal = "memberChoice";
-    const createTeam = () => {
-
+    const generateTeam = () =>{
         inquirer.prompt ([{
             type: "list",
-            name: newLocal,
+            name: "memberChoice",
             message: "Would you like to add any additional team members?",
             choices: ["Engineer", "Intern", "No further members to add"]
         }
-        ]).then(userChoice => {
-            switch (userChoice = newLocal) {
+        ]).then(userchoice => {
+            switch (userchoice.memberChoice) {
                 case "Engineer":
                     engineerQuestions();
                     break;
@@ -67,11 +63,10 @@ const Menu = () => {
                     internQuestions();
                     break;
                 default:
-                    console.log(buildTeam);
                     buildTeam();
             }          
         });
-    };
+    }
 
     const internQuestions = () => {
         return inquirer.prompt([
@@ -93,57 +88,59 @@ const Menu = () => {
 
             {type: "input",
             name: "internSchool",
-            message: "What is the intern's school name?",
+            message: "What is the intern's school name`?",
         },
 
 
         ]).then(answers => {
-            const intern = new Intern (answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
             teamMembers.push(intern);
             idArray.push(answers.internId);
-            createTeam();
+            generateTeam();
             });
-    };
+    }
     
     const engineerQuestions = () => {
         return inquirer.prompt([
     
                 {type: "input",
-                    name: "name",
-                    message: "What is the team manager's name?",
+                    name: "engineerName",
+                    message: "What is the engineer's name?",
                 },
     
                 {type: "input",
-                    name: "ID",
-                    message: "What is the manager's ID?",
+                    name: "engineerId",
+                    message: "What is the engineer's ID?",
                 },
     
                 {type: "input",
-                    name: "email",
-                    message: "What is the manager's email address?",
+                    name: "engineerEmail",
+                    message: "What is the engineer's email address?",
                 },
     
                 {type: "input",
-                name: "number",
-                message: "What is the manager's phone number?",
+                name: "engineerGithub",
+                message: "What is the engineer's github account?",
             },
     
     
         ]).then(answers => {
-            const engineer = new Engineer (answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice);
-            teamMembers.push(manager);
-            idArray.push(answers.managerId);
-            createTeam();
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            engineer.title = "Engineer"
+            console.table(engineer);
+            teamMembers.push(engineer);
+            idArray.push(answers.engineerId);
+            generateTeam();
             });
     };
 
 
 
     function buildTeam() {
-        if (!fs.existsSync(DIST_DIR)) {
-            fs.mkdirSync(DIST_DIR)
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
         }
-        fs.writeFileSync(outputPath, render(teamMember), "Checkout your team!");
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
     }
     generateQuestions();
 };
--- src/html-template.js
+++ src/html-template.js
@@ -1,59 +1,53 @@
 //Generates team
-
 const generateTeam = team => {
-
-    // Generate a manager card
+    //Generates the Manager HTML
     const generateManager = manager => {
         console.log(this)
         return `
-        <div class="card border-success mb-3" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-                <div class="card-body text-success">
-                    <h2 class="card-title">${manager.getName()}</h2>
-                        <h3 class="card-text">${manager.getTitle()}</h3>
-                </div>   
-            </div>     
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
-            </ul>
+        <div class="card border-success mb-3 manager">
+            <div class ="card-body text-success">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-text">${manager.getTitle()}</h3>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${manager.getName()}'s ID: ${manager.getId()}</li>
+                    <li class="list-group-item">${manager.getName()}'s Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">${manager.getName()}'s Office Number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
         </div>
     `};
+
     //Generates the Engineer HTML
     const generateEngineer = engineer => {
         return `
-        <div class="card border-success mb-3" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-                <div class="card-body text-success">
-                    <h2 class="card-title">${engineer.getName()}</h2>
-                        <h3 class="card-text">${engineer.getTitle()}</h3>
-                </div>   
-            </div>     
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: $engineer.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-                <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target-"_blank" rel-"nooepener noreferrer">${engineer.getGithub()}</a></li>
-            </ul>
+        <div class="card border-success mb-3 engineer">
+            <div class ="card-body text-success">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-text">${engineer.getTitle()}</h3>       
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${engineer.getName()}'s ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">${engineer.getName()}'s Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">${engineer.getName()}'s GitHub: <a href="https://github.com/${engineer.getGithub()}/" target-"_blank">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
         </div>
     `};
-    //Generates the Employee HTML
+
+    //Generates the Intern HTML
     const generateIntern = intern => {
         return `
-        <div class="card border-success mb-3" style="max-width: 18rem;">
-            <div class="card-header">Header</div>
-                <div class="card-body text-success">
-                    <h2 class="card-title">${intern.getName()}</h2>
-                        <h3 class="card-text">${intern.getTitle()}</h3>
-                </div>   
-            </div>     
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: $engineer.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-                <li class="list-group-item">School: ${intern.getSchool()}</li>
-            </ul>
+        <div class="card border-success mb-3 intern">
+            <div class ="card-body text-success">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-text">${intern.getTitle()}</h3>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${intern.getName()}'s ID: ${intern.getId()}</li>
+                    <li class="list-group-item">${intern.getName()}'s Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">${intern.getName()}'s School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
         </div>
-    `};
+`};
     const html = [];
     html.push(team
         .filter(employee => employee.getTitle() === "Manager")
@@ -65,8 +59,8 @@ const generateTeam = team => {
         .join("")
     );
     html.push(team
-        .filter(employee => employee.getTitle() === "Inter")
-        .map(engineer => generateIntern(intern))
+        .filter(employee => employee.getTitle() === "Intern")
+        .map(intern => generateIntern(intern))
         .join("")
     );
     console.log(html)
@@ -80,20 +74,19 @@ module.exports = team => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>${manager.project}</title>
+        <title>Team Builder</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <link rel="stylesheet" href="./dist/style.css">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
-                <h1 class="display-4">My Amazing Team</h1>
+                <h1 class="display-4">Team Profile</h1>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                <div class="team-card col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
+                    ${generateTeam(team)}
             </div>
         </div>
     </body>
